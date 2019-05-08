47c1a471486a7a3221a615ce5400c9c660a91640
Tomifish <978403854@qq.com>
2019-05-08 19:12:41 +0800

    测试添加

---
+++ src/main/test/hello.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: hello
+ * Author:   Administrator
+ * Date:     2019/5/8 19:09
+ * Description: hello
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+public class hello {
+    public static void main (String[] args){
+        System.out.println("hello");
+    }
+}
